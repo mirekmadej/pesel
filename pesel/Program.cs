5f1f7a8be0d2b9ac595c4589fa5d99cc0a51d600
Mirek Madej <m.madej@zsp3zamosc.pl>
2024-04-18 09:04:10 +0200

    gotowy

--- pesel/Program.cs
+++ pesel/Program.cs
@@ -7,7 +7,15 @@
         {
             this.pesel = pesel;
         }
-
+        /*****************************************
+         * nazwa funkcji: char sprawdzPlec(string pesel)
+         * opis funkcji:  funkcja dla danej wartości ciągu znaków
+         *                będącego numerem PESEL wyznacza płeć osoby
+         * parametry:     string reprezentujący numer PESEL
+         * zwracany typ i opis: typ char, wartości: 'K' dla peselu kobiety 
+         *                lub 'M' dla mężczyzny
+         * autor:         mm
+         * ***************************************/
         public char sprawdzPlec(string pesel)
         {
             int n = pesel[9] - '0';
@@ -18,8 +26,18 @@
         }
         public bool czyPoprawny(string pesel)
         {
-            
-            return false;
+            int[] wagi = new int[10] { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+            int suma = 0;
+            for (int i = 0; i < 10; i++)
+                suma += wagi[i] * int.Parse(pesel[i].ToString());
+            int m = suma % 10;
+            //Console.WriteLine(suma);
+            int r = 10 - m;
+            //Console.WriteLine(r);
+            if (r == pesel[10]-'0')
+                return true;
+            else
+                return false;
         }
         public override string ToString()
         {
@@ -30,11 +48,13 @@
     {
         static void Main(string[] args)
         {
-            string p = "55030101183";
+            string p = "55030101193";
             PESEL pesel = new PESEL(p);
             Console.WriteLine(pesel.ToString());
             Console.WriteLine(pesel.sprawdzPlec(p));
             Console.WriteLine(pesel.czyPoprawny(p));
+
+            
         }
     }
 }
